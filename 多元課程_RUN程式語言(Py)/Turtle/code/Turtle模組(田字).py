5d53473389fe2ec0cd7dc730d47a4fbe27c9d661
naiye130 <65766065+mdwiwi0130@users.noreply.github.com>
2022-05-23 21:23:50 +0800

    Create Turtle模組(田字).py

---
+++ 多元課程_RUN程式語言(Py)/Turtle/code/Turtle模組(田字).py
@@ -0,0 +1,9 @@
+import turtle
+turtle.clear()
+for i in range(0,4,1):
+  for i in range(0,4,1):
+    turtle.forward(50)
+    turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+turtle.done()
